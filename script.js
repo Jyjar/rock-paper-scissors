c5cbeb9b9c3462db10a673676680dd4ce9babc0c
Jyjar <156258612+Jyjar@users.noreply.github.com>
2024-07-29 15:20:18 +0200

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let hand = ["rock", "paper", "scissor"];
     randomNumber = Math.floor(Math.random() * 3);
+
     return hand[randomNumber];
 }
 
 function getHumanChoice() {
-    let choice = prompt("Type rock, paper or scissor.")
+    let choice = prompt("Type rock, paper or scissor.").toLowerCase();
+
     if(choice === "rock") {
         return "rock";
     } else if(choice === "paper") {
@@ -19,8 +24,55 @@ function getHumanChoice() {
     }
 }
 
-let computerChoice = getComputerChoice();
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
 
-let humanChoice = getHumanChoice();
+    if(humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                console.log(`Draw! ${humanChoice} vs ${computerChoice}.`);
+                break;
+            case "paper":
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                computerScore += 1;
+                break;
+            case "scissor":
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`) 
+                humanScore += 1;
+                break;
+        }
+    } else if(humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+                humanScore += 1;
+                break;
+            case "paper":
+                console.log(`Draw! ${humanChoice} vs ${computerChoice}.`);
+                break;
+            case "scissor":
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`); 
+                computerScore += 1;
+                break;
+        }
+    } else if(humanChoice === "scissor") {
+        switch (computerChoice) {
+            case "rock":
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                computerScore += 1;
+                break;
+            case "paper":
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+                humanScore += 1;
+                break;
+            case "scissor":
+                console.log(`Draw! ${humanChoice} vs ${computerChoice}.`);
+                break;
+        }
+    } else {
+        console.log("Error");
+    }
+}
 
-console.log(humanChoice)
+playRound()
