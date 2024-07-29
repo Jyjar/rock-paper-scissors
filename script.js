bd5c3a066b0c7a4722f83f74e88e776a5ea3689d
Jyjar <156258612+Jyjar@users.noreply.github.com>
2024-07-29 14:51:24 +0200

    Add the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 
 function getComputerChoice() {
-    hand = ["rock", "paper", "scissor"]
-    randomNumber = Math.floor(Math.random() * 3)
-    console.log(hand[randomNumber])
+    let hand = ["rock", "paper", "scissor"];
+    randomNumber = Math.floor(Math.random() * 3);
+    return hand[randomNumber];
 }
 
-getComputerChoice()
+function getHumanChoice() {
+    let choice = prompt("Type rock, paper or scissor.")
+    if(choice === "rock") {
+        return "rock";
+    } else if(choice === "paper") {
+        return "paper";
+    } else if(choice === "scissor"){
+        return "scissor";
+    } else {
+        console.log("Invalid input. Type rock, paper or scissor.");
+        return getHumanChoice();
+    }
+}
+
+let computerChoice = getComputerChoice();
+
+let humanChoice = getHumanChoice();
+
+console.log(humanChoice)
